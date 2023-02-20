cf51a05c2d5f8ac8f9a623450443906c72512b5a
Sofus Skovgaard <sofus.skovgaard@gmail.com>
2023-02-20 12:40:26 +0100

    feat: added gateway service

---
+++ App.Services.Gateway/Program.cs
@@ -0,0 +1,56 @@
+using App.Infrastructure.Extensions;
+using App.Services.Users.Infrastructure.Grpc;
+using Serilog;
+using Serilog.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.RegisterSerilog();
+
+// Add services to the container.
+
+builder.Services.RegisterOptions();
+
+builder.Services.AddRabbitMq();
+
+builder.Services.AddGrpcServiceClient<IUsersGrpcService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+    if (!app.Environment.IsDevelopment())
+    {
+        // Emit debug-level events instead of the defaults
+        options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+    }
+
+    // Attach additional properties to the request completion event
+    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+        diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+    };
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
