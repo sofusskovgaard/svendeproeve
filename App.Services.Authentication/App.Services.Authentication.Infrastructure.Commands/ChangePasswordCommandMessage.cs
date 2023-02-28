5df6b11750f4c98f308548272c641e3caa2e83b7
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-02-28 14:26:53 +0100

    feat: added commands and command handlers to authentication service

---
+++ App.Services.Authentication/App.Services.Authentication.Infrastructure.Commands/ChangePasswordCommandMessage.cs
@@ -0,0 +1,9 @@
+﻿using App.Infrastructure.Commands;
+
+namespace App.Services.Authentication.Infrastructure.Commands;
+
+public class ChangePasswordCommandMessage : ICommandMessage
+{
+    public string UserId { get; set; }
+    public string Password { get; set; }
+}
---
+++ App.Services.Authentication/App.Services.Authentication.Infrastructure.Commands/KillUserSessionCommandMessage.cs
@@ -0,0 +1,10 @@
+﻿using App.Infrastructure.Commands;
+
+namespace App.Services.Authentication.Infrastructure.Commands;
+
+public class KillUserSessionCommandMessage : ICommandMessage
+{
+    public string UserId { get; set; }
+
+    public string SessionId { get; set; }
+}
