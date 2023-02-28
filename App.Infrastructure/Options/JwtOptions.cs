bb836238e3447bee7a8c46dc0e22b86cc93c983d
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-02-28 14:29:19 +0100

    fix: updated jwt options

--- App.Infrastructure/Options/JwtOptions.cs
+++ App.Infrastructure/Options/JwtOptions.cs
@@ -5,5 +5,5 @@ public static class JwtOptions
     public static string Issuer => Environment.GetEnvironmentVariable("JWT_ISSUER") ?? "app";
     public static string Audience => Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? "app";
     public static string Key => Environment.GetEnvironmentVariable("JWT_SECRET_KEY") ?? "this_secret_has_to_be_at_least_128_bits_long";
-    public static uint TokenLifeTime => uint.TryParse(Environment.GetEnvironmentVariable("JWT_TOKEN_LIFE_TIME"), out var tokenLifeTime) ? tokenLifeTime : 1800;
+    public static int TokenLifeTime => int.TryParse(Environment.GetEnvironmentVariable("JWT_TOKEN_LIFE_TIME"), out var tokenLifeTime) ? tokenLifeTime : 1800;
 }
