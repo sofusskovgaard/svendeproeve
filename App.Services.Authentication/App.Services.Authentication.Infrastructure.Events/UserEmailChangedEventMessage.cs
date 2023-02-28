5fb6eadb417107b52cc5ba85f131a87e4ff68de0
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-02-28 14:27:16 +0100

    feat: added events to authentication service

---
+++ App.Services.Authentication/App.Services.Authentication.Infrastructure.Events/UserEmailChangedEventMessage.cs
@@ -0,0 +1,10 @@
+﻿using App.Infrastructure.Events;
+
+namespace App.Services.Authentication.Infrastructure.Events;
+
+public class UserEmailChangedEventMessage : IEventMessage
+{
+    public string UserId { get; set; }
+
+    public string Email { get; set; }
+}
---
+++ App.Services.Authentication/App.Services.Authentication.Infrastructure.Events/UserSessionKilledEventMessage.cs
@@ -0,0 +1,10 @@
+﻿using App.Infrastructure.Events;
+
+namespace App.Services.Authentication.Infrastructure.Events;
+
+public class UserSessionKilledEventMessage : IEventMessage
+{
+    public string UserId { get; set; }
+
+    public string SessionId { get; set; }
+}
