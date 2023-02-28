8d03763d92bdd6d6e5ee85a70dc26add19dfd7e8
Joachim Steen Tøfting <joac3109@elevcampus.dk>
2023-02-28 14:51:35 +0100

    feat: ticket order event added

---
+++ App.Services.Orders/App.Services.Orders.Infrastructure.Events/TicketOrderCreatedEventMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services.Orders.Infrastructure.Events
+{
+    public class TicketOrderCreatedEventMessage
+    {
+        public string UserId { get; set; }
+
+        public string OrderId { get; set; }
+
+        public string[] Tickets { get; set; }
+
+        public double total { get; set; }
+    }
+}
