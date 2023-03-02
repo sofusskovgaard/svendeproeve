a8ab86ffea7b969b1b0822002e4152cb02014e78
Allan Holm Iversen <alla1862@elevcampus.dk>
2023-03-02 13:23:38 +0100

    feat: solved #128 turnament created event handler

--- App.Services.Events/App.Services.Events.Data/Entities/EventEntity.cs
+++ App.Services.Events/App.Services.Events.Data/Entities/EventEntity.cs
@@ -13,7 +13,7 @@ public class EventEntity : BaseEntity
 {
     public string EventName { get; set; }
     public string Location { get; set; }
-    public IEnumerable<string> Tournaments { get; set; }
+    public string[] Tournaments { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 }
