59961a0bb002c9216e4dd5b91e6fb6c41ab4c6fa
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-03 11:00:03 +0100

    fix: whoops

--- App.Services.Events/App.Services.Events.Infrastructure.Commands/CreateEventCommandMessage.cs
+++ App.Services.Events/App.Services.Events.Infrastructure.Commands/CreateEventCommandMessage.cs
@@ -6,7 +6,7 @@ namespace App.Services.Events.Infrastructure.Commands
     {
         public string EventName { get; set; }
         public string Location { get; set; }
-        public IEnumerable<string> Tournaments { get; set; }
+        public string[] Tournaments { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
