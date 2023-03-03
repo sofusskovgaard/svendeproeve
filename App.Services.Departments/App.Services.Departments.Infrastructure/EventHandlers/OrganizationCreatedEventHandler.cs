8c7e8b35318c3f4f275945f2e8e0cd9609108dfa
Allan Holm Iversen <alla1862@elevcampus.dk>
2023-03-03 13:41:13 +0100

    feat: solved #163 subscribe to organization created event

---
+++ App.Services.Departments/App.Services.Departments.Infrastructure/EventHandlers/OrganizationCreatedEventHandler.cs
@@ -0,0 +1,36 @@
+﻿using App.Data.Services;
+using App.Infrastructure.Events;
+using App.Services.Departments.Data.Entities;
+using App.Services.Organizations.Infrastructure.Events;
+using MassTransit;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+
+namespace App.Services.Departments.Infrastructure.EventHandlers
+{
+    public class OrganizationCreatedEventHandler : IEventHandler<OrganizationCreatedEventMessage>
+    {
+        private readonly IEntityDataService _entityDataService;
+
+        public OrganizationCreatedEventHandler(IEntityDataService entityDataService)
+        {
+            _entityDataService = entityDataService;
+        }
+
+        public async Task Consume(ConsumeContext<OrganizationCreatedEventMessage> context)
+        {
+            var message = context.Message;
+
+            var department = await _entityDataService.GetEntity<DepartmentEntity>(message.DepartmentId);
+
+            List<string> organizations = new List<string>();
+            if (!department.OrganizationIds.IsNullOrEmpty())
+            {
+                organizations = department.OrganizationIds.ToList();
+            }
+            organizations.Add(message.Id);
+
+            var updateDefinition = new UpdateDefinitionBuilder<DepartmentEntity>().Set(entity => entity.OrganizationIds, organizations.ToArray());
+        }
+    }
+}
