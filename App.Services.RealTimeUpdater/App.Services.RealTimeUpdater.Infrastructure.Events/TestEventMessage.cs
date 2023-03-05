e4f225212a17ce0d04aeb4be1d9aa3cc59c3b1d4
Joachim Steen Tøfting <joac3109@elevcampus.dk>
2023-03-05 09:15:10 +0100

    feat: added signalr for realtime update

---
+++ App.Services.RealTimeUpdater/App.Services.RealTimeUpdater.Infrastructure.Events/TestEventMessage.cs
@@ -0,0 +1,8 @@
+﻿using App.Infrastructure.Events;
+
+namespace App.Services.RealTimeUpdater.Infrastructure.Events
+{
+    public class TestEventMessage : IEventMessage
+    {
+    }
+}
---
+++ RealTimeUpdater/RealTimeUpdater.Data/Entities/TestEntity.cs
@@ -0,0 +1,13 @@
+﻿using App.Data;
+using App.Data.Attributes;
+
+namespace RealTimeUpdater.Data.Entities
+{
+    [CollectionDefinition(nameof(TestEntity))]
+    public class TestEntity : BaseEntity
+    {
+        public string SomePublicProperty { get; set; }
+
+        public string SomeDetailedProperty { get; set; }
+    }
+}
