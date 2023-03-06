d6e29092b5e4813e7e967c80f7de8ee07f5ee7b2
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:54:15 +0100

    feat: added product search index

--- App.Services.Orders/App.Services.Orders.Data/Entities/ProductEntity.cs
+++ App.Services.Orders/App.Services.Orders.Data/Entities/ProductEntity.cs
@@ -3,12 +3,15 @@ using App.Data.Attributes;
 
 namespace App.Services.Orders.Data.Entities
 {
+    [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(ProductEntity))]
 
     public class ProductEntity : BaseEntity
     {
+        [IndexedProperty("search")]
         public string Name { get; set; }
 
+        [IndexedProperty("search")]
         public string Description { get; set; }
 
         public decimal Price { get; set; }
