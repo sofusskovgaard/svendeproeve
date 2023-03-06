d7b9b85f8687d81561423ac2f2eac3b34e19eafc
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 11:22:14 +0100

    feat: added indexes to team entities

--- App.Services.Teams/App.Services.Teams.Data/Entities/TeamEntity.cs
+++ App.Services.Teams/App.Services.Teams.Data/Entities/TeamEntity.cs
@@ -5,6 +5,10 @@ namespace App.Services.Teams.Data.Entities
 {
     [SearchIndexDefinition("search")]
     [IndexDefinition("name", isUnique: true)]
+    [IndexDefinition("game")]
+    [IndexDefinition("members")]
+    [IndexDefinition("manager")]
+    [IndexDefinition("organization")]
     [CollectionDefinition(nameof(TeamEntity))]
     public class TeamEntity : BaseEntity
     {
@@ -15,9 +19,13 @@ namespace App.Services.Teams.Data.Entities
         public string Bio { get; set; }
         public string ProfilePicturePath { get; set; }
         public string CoverPicturePath { get; set; }
+        [IndexedProperty("game")]
         public string GameId { get; set; }
+        [IndexedProperty("members")]
         public string[] MembersId { get; set; }
+        [IndexedProperty("manager")]
         public string ManagerId { get; set; }
+        [IndexedProperty("organization")]
         public string OrganizationId { get; set; }
     }
 }
