9ec23951d9ad3029c1be70c11df246910e80b461
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 11:21:37 +0100

    feat: added indexes to order charge entities

--- App.Services.Billing/App.Services.Billing.Data/Entities/OrderChargeEntity.cs
+++ App.Services.Billing/App.Services.Billing.Data/Entities/OrderChargeEntity.cs
@@ -3,11 +3,15 @@ using App.Data.Attributes;
 
 namespace App.Services.Billing.Data.Entities;
 
+[IndexDefinition("order")]
+[IndexDefinition("status")]
 [CollectionDefinition(nameof(OrderChargeEntity))]
 public class OrderChargeEntity : BaseEntity
 {
+    [IndexedProperty("order")]
     public string OrderId { get; set; }
 
+    [IndexedProperty("status")]
     public string Status { get; set; }
 
     public decimal Amount { get; set; }
