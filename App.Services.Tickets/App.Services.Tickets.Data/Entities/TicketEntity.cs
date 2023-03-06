3656707985ae3e926e951c8ee38ec3e29e1318b6
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 11:22:19 +0100

    feat: added indexes to ticket entities

--- App.Services.Tickets/App.Services.Tickets.Data/Entities/TicketEntity.cs
+++ App.Services.Tickets/App.Services.Tickets.Data/Entities/TicketEntity.cs
@@ -9,18 +9,26 @@ using App.Services.Tickets.Common;
 
 namespace App.Services.Tickets.Data.Entities
 {
+    [IndexDefinition("product")]
+    [IndexDefinition("buyer")]
+    [IndexDefinition("status")]
+    [IndexDefinition("order")]
     [CollectionDefinition(nameof(TicketEntity))]
 
     public class TicketEntity : BaseEntity
     {
+        [IndexedProperty("product")]
         public string ProductId { get; set; }
 
+        [IndexedProperty("buyer")]
         public string BuyerId { get; set; }
 
         public string Recipient { get; set; }
 
+        [IndexedProperty("status")]
         public string Status { get; set; } = TicketStatus.Booked;
 
+        [IndexedProperty("order")]
         public string? OrderId { get; set; }
     }
 }
