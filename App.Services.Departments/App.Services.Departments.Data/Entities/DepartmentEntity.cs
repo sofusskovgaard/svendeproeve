58b24572ed8c4ad2c79d38b01ec5ee9f817a42d4
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:50:02 +0100

    feat: add search index to departments

--- App.Services.Departments/App.Services.Departments.Data/Entities/DepartmentEntity.cs
+++ App.Services.Departments/App.Services.Departments.Data/Entities/DepartmentEntity.cs
@@ -3,11 +3,14 @@ using App.Data.Attributes;
 
 namespace App.Services.Departments.Data.Entities;
 
+[SearchIndexDefinition("search")]
 [CollectionDefinition(nameof(DepartmentEntity))]
 public class DepartmentEntity : BaseEntity
 {
+    [IndexedProperty("search", 2)]
     public string Name { get; set; }
 
+    [IndexedProperty("search")]
     public string Address { get; set; }
 
     public string[] OrganizationIds { get; set; }
