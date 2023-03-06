76a53b816cb578c90c2001e15ce78430b9567761
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 11:22:04 +0100

    feat: added indexes to product entities

--- App.Services.Orders/App.Services.Orders.Data/Entities/ProductEntity.cs
+++ App.Services.Orders/App.Services.Orders.Data/Entities/ProductEntity.cs
@@ -3,6 +3,7 @@ using App.Data.Attributes;
 
 namespace App.Services.Orders.Data.Entities
 {
+    [IndexDefinition("shard")]
     [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(ProductEntity))]
 
@@ -16,8 +17,10 @@ namespace App.Services.Orders.Data.Entities
 
         public decimal Price { get; set; }
 
+        [IndexedProperty("shard")]
         public string? ReferenceId { get; set; }
 
+        [IndexedProperty("shard", true)]
         public string? ReferenceType { get; set; }
     }
 }
