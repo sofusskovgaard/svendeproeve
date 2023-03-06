eb9087e87291e6d68a18753ba83226cbadc0e475
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:54:30 +0100

    feat: added team search index

--- App.Services.Teams/App.Services.Teams.Data/Entities/TeamEntity.cs
+++ App.Services.Teams/App.Services.Teams.Data/Entities/TeamEntity.cs
@@ -3,12 +3,15 @@ using App.Data.Attributes;
 
 namespace App.Services.Teams.Data.Entities
 {
+    [SearchIndexDefinition("search")]
     [IndexDefinition("name", isUnique: true)]
     [CollectionDefinition(nameof(TeamEntity))]
     public class TeamEntity : BaseEntity
     {
+        [IndexedProperty("search")]
         [IndexedProperty("name")]
         public string Name { get; set; }
+        [IndexedProperty("search")]
         public string Bio { get; set; }
         public string ProfilePicturePath { get; set; }
         public string CoverPicturePath { get; set; }
