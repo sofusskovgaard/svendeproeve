d7b8b7344f899f7c1a77289c32efb5fa7bffe302
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:54:23 +0100

    feat: added organization search index

--- App.Services.Organizations/App.Services.Organizations.Data/Entities/OrganizationEntity.cs
+++ App.Services.Organizations/App.Services.Organizations.Data/Entities/OrganizationEntity.cs
@@ -3,15 +3,19 @@ using App.Data.Attributes;
 
 namespace App.Services.Organizations.Data.Entities
 {
+    [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(OrganizationEntity))]
     public class OrganizationEntity : BaseEntity
     {
+        [IndexedProperty("search")]
         public string Name { get; set; }
+        [IndexedProperty("search")]
         public string? Bio { get; set; }
         public string? ProfilePicture { get; set; }
         public string? CoverPicture { get; set; }
         public string[]? MemberIds { get; set; }
         public string[]? TeamIds { get; set; }
+        [IndexedProperty("search")]
         public string? Address { get; set; }
         public string? DepartmentId { get; set; }
     }
