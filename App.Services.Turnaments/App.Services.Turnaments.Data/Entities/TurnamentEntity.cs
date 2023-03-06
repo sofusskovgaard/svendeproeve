7d7cef711e132d82b125299dc6484673787d464e
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:54:50 +0100

    feat: added tournament search index

--- App.Services.Turnaments/App.Services.Turnaments.Data/Entities/TurnamentEntity.cs
+++ App.Services.Turnaments/App.Services.Turnaments.Data/Entities/TurnamentEntity.cs
@@ -3,9 +3,11 @@ using App.Data.Attributes;
 
 namespace App.Services.Turnaments.Data.Entities
 {
+    [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(TurnamentEntity))]
     public class TurnamentEntity : BaseEntity
     {
+        [IndexedProperty("search")]
         public string Name { get; set; }
         public string GameId { get; set; }
         public string[] MatchesId { get; set; }
