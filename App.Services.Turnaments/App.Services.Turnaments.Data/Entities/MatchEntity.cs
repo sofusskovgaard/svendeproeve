cbae1308979971a7f1c2f6365c21a5b32194828a
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 11:22:23 +0100

    feat: added indexes to match entities

--- App.Services.Turnaments/App.Services.Turnaments.Data/Entities/MatchEntity.cs
+++ App.Services.Turnaments/App.Services.Turnaments.Data/Entities/MatchEntity.cs
@@ -3,14 +3,20 @@ using App.Data.Attributes;
 
 namespace App.Services.Turnaments.Data.Entities
 {
+    [IndexDefinition("tournament")]
+    [IndexDefinition("teams")]
+    [IndexDefinition("winning_team")]
     [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(MatchEntity))]
     public class MatchEntity : BaseEntity
     {
         [IndexedProperty("search")]
         public string Name { get; set; }
+        [IndexedProperty("teams")]
         public string[] TeamsId { get; set; }
+        [IndexedProperty("tournament")]
         public string TurnamentId { get; set; }
+        [IndexedProperty("winning_team")]
         public string WinningTeamId { get; set; }
     }
 }
