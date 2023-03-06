ac7566fb9ec417f7cf9d9ad668e95b28a1001404
Sofus Skovgaard <hello@sofusskovgaard.com>
2023-03-06 09:54:41 +0100

    feat: added match search index

--- App.Services.Turnaments/App.Services.Turnaments.Data/Entities/MatchEntity.cs
+++ App.Services.Turnaments/App.Services.Turnaments.Data/Entities/MatchEntity.cs
@@ -3,9 +3,11 @@ using App.Data.Attributes;
 
 namespace App.Services.Turnaments.Data.Entities
 {
+    [SearchIndexDefinition("search")]
     [CollectionDefinition(nameof(MatchEntity))]
     public class MatchEntity : BaseEntity
     {
+        [IndexedProperty("search")]
         public string Name { get; set; }
         public string[] TeamsId { get; set; }
         public string TurnamentId { get; set; }
